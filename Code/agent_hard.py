f8332c5c48e8b9a486998c33ede7debc6ef88422
pitch <95540893+pitch2@users.noreply.github.com>
2024-07-03 23:04:21 +0200

    Update agent_hard.py

--- Code/agent_hard.py
+++ Code/agent_hard.py
@@ -1,15 +1,4 @@
 import subprocess
-import mysql.connector
-
-
-#Connexion à la BDD
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="agent"
-)
-
 
 def hardware():
   #Informations que nous voulons récupérer (voir note pour comprendre)
@@ -44,20 +33,6 @@ def hardware():
   return liste
 
 
-hardware_liste = hardware()
-
-    
-
-
-# Envoie des données vers la BDD
-mycursor = mydb.cursor()
-sql = "INSERT INTO Hardware (Hostname, CPU, GPU, RAM, BaseBoard, DiskDrive_Model, DiskDrive_Size, DiskDrive_State) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-val = (hardware_liste[0], hardware_liste[1], hardware_liste[2], hardware_liste[3], hardware_liste[4], hardware_liste[5], hardware_liste[6], hardware_liste[7])
-mycursor.execute(sql, val)
-mydb.commit()
-
-
-print(mycursor.rowcount, "Validé")
 
 
 
