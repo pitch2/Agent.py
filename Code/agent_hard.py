4f618cdd74f8c44b0c0f316f6897155679e6e529
pitch <95540893+pitch2@users.noreply.github.com>
2024-07-03 22:59:26 +0200

    Update agent_hard.py

--- Code/agent_hard.py
+++ Code/agent_hard.py
@@ -11,44 +11,48 @@ mydb = mysql.connector.connect(
 )
 
 
+def hardware():
+  #Informations que nous voulons récupérer (voir note pour comprendre)
+  dictionnaire, liste = {
+      "Win32_Processor": ["Name"],
+      "Win32_VideoController": ["VideoProcessor"],
+      "Win32_PhysicalMemory": ["__PROPERTY_COUNT"],
+      "Win32_BaseBoard": ["Product"],
+      "Win32_DiskDrive": ["Model", "Size", "Status"]
+  }, []
 
-#Informations que nous voulons récupérer (voir note pour comprendre)
-dictionnaire, liste = {
-    "Win32_Processor": ["Name"],
-    "Win32_VideoController": ["VideoProcessor"],
-    "Win32_PhysicalMemory": ["__PROPERTY_COUNT"],
-    "Win32_BaseBoard": ["Product"],
-    "Win32_DiskDrive": ["Model", "Size", "Status"]
-}, []
+  # Récupération des informations du PC en Powershell
+  # Récupération du nom de la machine
 
-# Récupération des informations du PC en Powershell
-# Récupération du nom de la machine
+  process = subprocess.Popen(["powershell", "-Command", "hostname"], stdout=subprocess.PIPE)
+  output, _ = process.communicate()
+  liste.append(output.decode("utf-8").split("\r")[0])
 
-process = subprocess.Popen(["powershell", "-Command", "hostname"], stdout=subprocess.PIPE)
-output, _ = process.communicate()
-liste.append(output.decode("utf-8").split("\r")[0])
+  # Récupération des informations du dico
+  for classe, proprietes in dictionnaire.items():
+      for propriete in proprietes:
+        command = f"Get-WmiObject -Class {classe} | Select-Object -Property {propriete} -Unique | Format-Table -HideTableHeaders"
+        # Exécuter la commande PowerShell et capturer la sortie
+        process = subprocess.Popen(["powershell", "-Command", command], stdout=subprocess.PIPE)
 
-# Récupération des informations du dico
-for classe, proprietes in dictionnaire.items():
-    for propriete in proprietes:
-      command = f"Get-WmiObject -Class {classe} | Select-Object -Property {propriete} -Unique | Format-Table -HideTableHeaders"
-      # Exécuter la commande PowerShell et capturer la sortie
-      process = subprocess.Popen(["powershell", "-Command", command], stdout=subprocess.PIPE)
+        # Décoder la sortie et l'afficher
+        output, _ = process.communicate()
 
-      # Décoder la sortie et l'afficher
-      output, _ = process.communicate()
+        #print((((output.decode("utf-8").split("\r"))[1]).split("\n"))[1])
+        liste.append((((output.decode("utf-8").split("\r"))[1]).split("\n"))[1])
+  
+  return liste
 
-      #print((((output.decode("utf-8").split("\r"))[1]).split("\n"))[1])
-      liste.append((((output.decode("utf-8").split("\r"))[1]).split("\n"))[1])
 
-print(liste)
+hardware_liste = hardware()
+
     
 
 
 # Envoie des données vers la BDD
 mycursor = mydb.cursor()
 sql = "INSERT INTO Hardware (Hostname, CPU, GPU, RAM, BaseBoard, DiskDrive_Model, DiskDrive_Size, DiskDrive_State) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-val = (liste[0], liste[1], liste[2], liste[3], liste[4], liste[5], liste[6], liste[7])
+val = (hardware_liste[0], hardware_liste[1], hardware_liste[2], hardware_liste[3], hardware_liste[4], hardware_liste[5], hardware_liste[6], hardware_liste[7])
 mycursor.execute(sql, val)
 mydb.commit()
 
